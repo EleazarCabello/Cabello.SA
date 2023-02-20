10423e0127532e89369c922faf7c3ce357a53d42
EleazarCabello <L19100114@nlaredo.tecnm.mx>
2023-02-20 01:12:12 -0600

    .

---
+++ Eventos/Eventos3.js
@@ -0,0 +1,25 @@
+let events = require('events').EventEmitter;
+
+let util = require('util');
+
+
+function saludar(){
+
+    setTimeout(
+    ()=>{
+    this.emit('iniciaSaludo');
+    console.log('Hola Mundo');
+    this.emit('terminaSaludo');
+    },2000);
+    return this;
+}
+
+util.inherits(saludar,events)
+let sal= new saludar();
+
+sal.on('iniciaSaludo',function(){
+    console.log('La funcion saludar emitio el evento iniciaSaludo')
+})
+sal.on('terminaSaludo',function(){
+    console.log('La funcion saludar emitio el evento terminaSaludo')
+})
