95c6bb9f7493491ea72bb569f7b9be9f1cb81432
EleazarCabello <L19100114@nlaredo.tecnm.mx>
2023-02-24 15:31:50 -0600

    .

--- Parcial1/ServidorExpressDB/index.js
+++ Parcial1/ServidorExpressDB/index.js
@@ -22,9 +22,41 @@ app.get('/id/:id',async(req,res)=>{
     }else{
         res.json(rows);
     }
-  
 })
 
+app.post('/id',async(req,res)=>{
+    let sentenciaSql = `insert into mundo values(${req.body.Id},'${req.body.Usuario}','${req.body.Edad}')`;
+    const connection = await mysql.createConnection({host:'localhost', user: 'root', database: 'world'});
+    const [rows, fields] = await connection.execute(sentenciaSql);
+
+    
+    if(rows.affectedRows == 1){
+        res.json({status:"Insercion realizada con exito"});
+    }
+})
+
+
+app.delete('/id/:id',async(req,res)=>{
+
+    const connection = await mysql.createConnection({host:'localhost', user: 'root', database: 'world'});
+    const [rows, fields] = await connection.execute('DELETE FROM mundo WHERE Id = ? ',[req.params.id]);
+
+    res.json(rows)
+    if(rows.affectedRows == 1){
+        res.json({status:"Registro eliminado"});
+    }
+})
+
+app.patch('/id/:id',async(req,res)=>{
+
+    const connection = await mysql.createConnection({host:'localhost', user: 'root', database: 'world'});
+    const [rows, fields] = await connection.execute(`UPDATE mundo SET Usuario = '${req.body.Usuario}', Edad = ${req.body.Edad} WHERE Id = ? `,[req.params.id]);
+
+    //res.json(rows)
+    if(rows.affectedRows == 1){
+        res.json({status:"Registro actualizado"});
+    }
+})
 
 app.use((req,res)=>{
     res.status(404).json({estado:"Pagina no encontrada"})
