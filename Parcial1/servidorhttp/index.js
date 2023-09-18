c04daf5dbe436aab3c9e58a19ec8e1204a77f283
EleazarCabello <L19100114@nlaredo.tecnm.mx>
2023-09-18 15:59:19 -0600

    .

--- Parcial1/servidorhttp/index.js
+++
@@ -1,7 +0,0 @@
-let http = require('http');
-
-let servidor = http.createServer(function (req, res) {
-res.write('Servidor HTTP contestando al 100%');
-res.end();
-})
-servidor.listen(8081, () => console.log('Servidor listo y contestando'));
--- Parcial2/Jsdoc/src/index.js
+++
@@ -1,3 +0,0 @@
-import * as mate from "./mate.js";
-console.log(mate.areaCuadrado(2));
-mate.areaCirculo();
