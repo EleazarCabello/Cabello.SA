ef4593b8d47d9124f39cd3f100752c4ef3344ea6
EleazarCabello <L19100114@nlaredo.tecnm.mx>
2023-09-18 16:01:30 -0600

    .

--- Parcial2/Router/indexRouterExterno.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express();
-const empleado=require('./empleado.js')
-
-app.use(empleado.router);
-
-app.listen(8082,(req,resp)=>{
-    console.log("Servidor escuchando")
-})
